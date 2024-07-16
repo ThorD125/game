dead9a5aaccd4a31dfde755ab9bd488a225e89f1
Thor <thor.demeestere@telenet.be>
2024-07-16 22:02:40 +0200

    fixed collision

--- classes/Bird.lua
+++ classes/Bird.lua
@@ -20,17 +20,28 @@ end
 function Bird:collides(pipe)
     birdTop = self.y
     birdBottom = self.y + self.height
+    birdLeft = self.x
+    birdRight = birdLeft + self.width
 
-    gapTop = pipe.y
-    gapBottom = pipe.y + pipe.gapHeight
+    gapTop = pipe.y - pipe.gapHeight
+    gapBottom = pipe.y 
+    
+    pipeLeft = pipe.x
+    pipeRight = pipe.x + pipe.width
 
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + pipe.width then
 
 
+    if birdRight >= pipeLeft and birdLeft <= pipeRight then
+        if birdBottom > gapBottom then
+            return true
+        elseif gapTop > birdTop then
+            return true
+        end
     end
 
     return false
 end
+
 
 function Bird:render()
     love.graphics.draw(imgBird, self.x, self.y)
